8e627ecc5ea1f74af21e8a7c1d4004c103d8fb84
amirul asrof <acop95@gmail.com>
2019-05-24 11:18:01 +0800

    second commit

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -4,27 +4,27 @@ import { Logo } from "../components/header";
 import { HeaderBackButton } from "react-navigation";
 
 export default class SettingsScreen extends React.Component {
-    static navigationOptions = ({ navigation }) => {
-        return {
-            headerLeft: <HeaderBackButton onPress={() => navigation.goBack(null)} />,
-            headerTitle: <Logo />,
-            headerBackTitle: "Settings",
-            headerLayoutPreset: "center"
-        };
-    };
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text>Hello! Welcome to my settings page</Text>
-            </View>
-        );
-    }
+	static navigationOptions = ({ navigation }) => {
+		return {
+			headerLeft: <HeaderBackButton onPress={() => navigation.goBack(null)} />,
+			headerTitle: <Logo />,
+			headerBackTitle: "Settings",
+			headerLayoutPreset: "center"
+		};
+	};
+	render() {
+		return (
+			<View style={styles.container}>
+				<Text>Hello! Welcome to my settings page</Text>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center"
-    }
+	container: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center"
+	}
 });
